aaf547f771943608f8001679a3a2e5da3266e95f
S3N1OR <a1iev@internet.ru>
2023-03-06 22:48:12 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$('.slider__list').slick({
+    slidesToShow: 3,
+    variableWidth: true,
+    infinite: false,
+    slidesToScroll: 1
+});
+
+$('.catigories-slider').slick({
+    slidesToShow: 3,
+    variableWidth: true,
+    infinite: false,
+    slidesToScroll: 1
+});
